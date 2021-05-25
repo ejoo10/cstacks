e0332f927d0747219e4db4b5f7ff293fb9af45f0
ejoo10 <78502972+ejoo10@users.noreply.github.com>
2021-05-25 15:51:24 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from matrix import *
+from draw import *
+from display import *
+from parse import *
+import math
+import random
+
+s = new_screen()
+c = [ 255, 192, 203 ]
+
+edges = [[], [], [], []]
+triangles = [[], [], [], []]
+m = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
+cs = [m]
+
+
+parse('script', edges, triangles, cs, s, c)
